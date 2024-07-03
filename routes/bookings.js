188caf9169d5e358c72d4dec778d3c4b6c44d250
patleserdev <leserpat@hotmail.com>
2024-07-03 11:57:36 +0200

    add multiple booking

--- routes/bookings.js
+++ routes/bookings.js
@@ -29,6 +29,42 @@ router.post('/add', (req, res) => {
         res.json({result:false,error:'Champs manquants ou invalides'})
     }
 })
+
+/**
+ * 	Ajout plusieurs trajets au panier 
+ * 	{result}
+ */
+router.post('/addMany', (req, res) => {
+    if (checkBody(req.body,['tripId']))
+    {
+        let trips=req.body.tripIds
+        let counter=0
+        for(let trip of trips)
+        {
+            let newBooking= new Booking({trip : trip}) 
+            newBooking.save().then((data) => {
+                console.log(data)
+                counter++
+            })
+        }
+      
+       
+            if(counter != null)
+            {
+                res.json({result:true})
+            }
+            else
+            {
+                res.json({result:false,error:'Erreur lors des enregistements'})
+            }        
+       
+    }
+    else
+    {
+        res.json({result:false,error:'Champs manquants ou invalides'})
+    }
+})
+
 	
 /***
  * 	Récupérer tous les éléments du booking
